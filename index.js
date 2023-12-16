41868e263546b1156056f5566a783c4a374a7e40
tlochsta <81081702+TlochstaDev@users.noreply.github.com>
2023-12-16 13:13:27 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
+import Easyviolet from 'easyviolet';
+import http from 'node:http';
+import express from 'express';
 
+const server = http.createServer();
+const app = express();
+
+
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views');
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+new Easyviolet({
+  server: server
+});
+
+server.listen(8080, () => {
+  console.log(`Your easyviolet demo is running on port ${server.address().port}`);
+});
